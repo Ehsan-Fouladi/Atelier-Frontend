44957d98af037940500833303d5f0738090e2368
amirali-yavari <amiraliyavari785@gmail.com>
2024-04-05 05:44:40 +0330

    feat: make radialProgress comp

---
+++ components/radialprogres.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        :class="'radial-progress ' + bgColor + ' ' + 'text-[' + strokeColor + ']' "
+        :style="'--value:' + valueProps + '; ' + '--size:' + sizeProps + '; ' + '--thickness:' + strokeWidth + ';'" 
+        role="progressbar"
+    >
+        {{ strokeColor }}
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    valueProps: { type: Number, required: true },
+    sizeProps: { type: String, required: true },
+    bgColor: { type: String, default: 'bg-base-200' },
+    strokeWidth: { type: String, default: '4px' },
+    strokeColor: { type: String, default: '#0A65CD' }
+})
+</script>
