32796246647763c730943c30374cdc784efe7d50
Ehsan Fouladi <fooehan3@gmail.com>
2024-04-24 21:11:58 +0330

    create header update page home add image logo

---
+++ layouts/default.vue
@@ -0,0 +1,66 @@
+<template>
+  <header class="flex w-full h-32">
+    <nav class="flex flex-1 justify-around items-center w-full shadow-lg">
+      <ul class="cursor-pointer hidden lg:flex items-center space-x-5 uppercase">
+        <li class="hover:text-yellow-800 duration-300 flex items-center gap-2 text-gray-600">
+          <a href="#" class="font-medium">home</a>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+          </svg>
+        </li>
+        <li class="hover:text-yellow-800 duration-300 flex items-center gap-2 text-gray-600">
+          <a href="#" class="font-medium">pages</a>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+          </svg>
+        </li>
+        <li class="hover:text-yellow-800 duration-300 flex items-center gap-2 text-gray-600">
+          <a href="#" class="font-medium">shop</a>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+          </svg>
+        </li>
+        <li class="hover:text-yellow-800 duration-300 flex items-center gap-2 text-gray-600">
+          <a href="#" class="font-medium">news</a>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+          </svg>
+        </li>
+      </ul>
+      <div class="hidden sm:block lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </div>
+      <img class="object-cover max-w-[135px]" src="/img/logo.png" alt="logo">
+      <ul class="cursor-pointer flex items-center space-x-5 uppercase">
+        <li class="hover:text-yellow-800 duration-300 flex items-center gap-2 text-gray-600">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+          </svg>
+          <a href="#" class="font-medium hidden lg:block">account</a>
+        </li>
+        <li class="hover:text-yellow-800 duration-300 flex items-center gap-2 text-gray-600">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+          </svg>
+          <a href="#" class="font-medium hidden lg:block">Wishlist</a>
+        </li>
+        <li class="hover:text-yellow-800 duration-300 flex items-center gap-2 text-gray-600">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+          </svg>
+          <a href="#" class="font-medium hidden lg:block">cart</a>
+          <a href="#" class="font-medium">(0)</a>
+        </li>
+      </ul>
+      <div class="sm:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </div>
+    </nav>
+  </header>
+</template>
+<script setup>
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template>
     <div class="flex flex-col w-full h-svh">
         <h1 class="text-4xl font-bold mx-auto my-20">Great men starting a new project</h1>
-        <Radialprogress class="mx-auto" :sizeProps="'10rem'" :valueProps="80" :bgColor="'bg-green-200'" :strokeWidth="'10px'">
-            test
-        </Radialprogress>
     </div>
 </template>
