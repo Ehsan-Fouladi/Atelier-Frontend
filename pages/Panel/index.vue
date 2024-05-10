bc952c644a136fee7b10bda505eccf71d4f579dd
Ehsan Fouladi <fooehan3@gmail.com>
2024-05-10 23:53:43 +0330

    added panel user shop

---
+++ pages/Panel/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-gray-200 h-screen flex justify-center items-center place-content-center">
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'panel'
+})
+</script>
