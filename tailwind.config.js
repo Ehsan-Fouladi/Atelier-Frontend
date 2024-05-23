788cf505112c46bb5cc9a138c5f2493809bcef04
Ehsan Fouladi <fooehan3@gmail.com>
2024-05-23 15:38:58 +0330

    update custom color daisui in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,36 +15,22 @@ export default {
   daisyui: {
     themes: [
       {
-        light: {
+        halloween: {
           ...require("daisyui/src/theming/themes")["halloween"],
-          ".btn-order": {
-            "background-color": "#6f4ef2",
-            "border-radius": "0.75rem",
-            "padding-left":"1rem",
-            "padding-right":"1rem",
-            "padding-top": "0.75rem",
-            "padding-bottom": "0.75rem",
-            "color": "white",
-            "border-color": "#6f4ef2",
-          },
-          ".btn-order:hover": {
-            "background-color": "#572dff",
-            "border-color": "#572dff",
-          },
-          ".btn-delete": {
-            "background-color": "#f73164",
-            "border-radius": "0.75rem",
-            "padding-left":"1rem",
-            "padding-right":"1rem",
-            "padding-top": "0.75rem",
-            "padding-bottom": "0.75rem",
-            "color": "white",
-            "border-color": "#f73164",
-          },
-          ".btn-delete:hover": {
-            "background-color": "#ff1751",
-            "border-color": "#ff1751",
-          },
+          primary: "blue",
+          "primary-content": "white",
+          error: "red",
+          "error-content": "white",
+          warning: "yellow",
+          "warning-content": "black",
+          success: "green",
+          "success-content": "white",
+          info: "cyan",
+          "info-content": "black",
+          accent: "orange",
+          "accent-content": "black",
+          neutral: "black",
+          "neutral-content": "white",
         },
       }
     ]
